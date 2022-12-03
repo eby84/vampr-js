8f5759f16c56c8f5aac24323f68af8f911b929eb
Ebele <ebele.omanukwue@telus.com>
2022-12-02 19:28:39 -0700

    updates

--- vampire.js
+++ vampire.js
@@ -10,21 +10,40 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
+    let offSprings = this.offspring.length;
+    return offSprings;
 
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let Vampires = 0;
+
+    let parent = this;
+
+    while (parent.creator) {
+
+      parent = parent.creator;
+      Vampires++;
+    }
+    return Vampires;
+
 
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
+    if (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal) {
+      return true;
+    }
+
+    return false;
 
   }
 
@@ -36,9 +55,18 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
-
+    let currentVampire = this;
+    if (!currentVampire.creator) {
+      return currentVampire;
+    }
+    while (currentVampire.creator) {
+      if (currentVampire.creator === vampire.creator) {
+        return currentVampire.creator;
+      }
+    }
   }
 }
+
 
 module.exports = Vampire;
 
